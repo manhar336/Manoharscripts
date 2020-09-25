71f0944f4c074496266b7337c5b3748ae09dcf1f
Manohar Dharmala <manhar336@gmail.com>
2020-09-25 09:57:28 -0700

    local:updating mctautorecover.py file

--- mctautorecover.py
+++ mctautorecover.py
@@ -1,4 +1,4 @@
-# /usr/bin/python  This is comment and adding for automation commit
+# /usr/bin/python  This is comment and adding for automation commit updating in local branch
 #purpose:script used to recover group servers automatically after 30 mins in MCT
 #Date: 24-09-2020
 import requests
